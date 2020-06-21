76f9960cb1b44aaa0aed8870f29a778037665ef0
Edd-wordd <edward.plasencio@gmail.com>
2020-06-20 22:45:10 -0700

    redesign singin.js

--- src/components/pages/SignIn.js
+++ src/components/pages/SignIn.js
@@ -2,22 +2,26 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Navigation from "../ui/Navigation";
 import AppTemplate from "../ui/AppTemplate";
+import SignUp from "../ui/SignUp";
 
 export default function SignIn() {
    return (
       <div>
          <Navigation />
          <AppTemplate>
-            <div className="col-6 offset-3 text-center ">
+            <div className="col-4 offset-4 text-center ">
                <form>
                   <h3>Lets Sign In</h3>
                   <input
-                     className="mb-3 w-75"
-                     placeholder="enter email"
+                     className="form-control"
+                     placeholder="Enter Email"
                   ></input>
                   <br />
 
-                  <input className="mb-2 w-75" placeholder="password"></input>
+                  <input
+                     className="mb-2 form-control"
+                     placeholder="Enter Password"
+                  ></input>
                   <div className="clerfix"></div>
                   <Link to="/home" className="btn btn-primary">
                      Sign Up
