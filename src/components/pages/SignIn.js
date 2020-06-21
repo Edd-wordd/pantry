dc9674af0c5738e7ab5c35801c1f2f4fe2b6f4b3
Edd-wordd <edward.plasencio@gmail.com>
2020-06-20 23:12:54 -0700

    add signin.js validation

--- src/components/pages/SignIn.js
+++ src/components/pages/SignIn.js
@@ -1,8 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Navigation from "../ui/Navigation";
 import AppTemplate from "../ui/AppTemplate";
-import SignUp from "../ui/SignUp";
+import SignInUi from "../ui/SignInUi";
 
 export default function SignIn() {
    return (
@@ -10,23 +9,7 @@ export default function SignIn() {
          <Navigation />
          <AppTemplate>
             <div className="col-4 offset-4 text-center ">
-               <form>
-                  <h3>Lets Sign In</h3>
-                  <input
-                     className="form-control"
-                     placeholder="Enter Email"
-                  ></input>
-                  <br />
-
-                  <input
-                     className="mb-2 form-control"
-                     placeholder="Enter Password"
-                  ></input>
-                  <div className="clerfix"></div>
-                  <Link to="/home" className="btn btn-primary">
-                     Sign Up
-                  </Link>
-               </form>
+               <SignInUi />
             </div>
          </AppTemplate>
       </div>
---
+++ src/components/ui/SignInUi.js
@@ -0,0 +1,145 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import classnames from "classnames";
+import hash from "object-hash";
+import { v4 as getUuid } from "uuid";
+
+class SignInUi extends React.Component {
+   constructor(props) {
+      super(props);
+      console.log("edward is here");
+      this.state = {
+         isSignUpClicked: false,
+         emailerror: "",
+         passwordError: "",
+         hasEmailError: false,
+         hasPasswordError: false,
+      };
+   }
+   async setEmailState(emailInput) {
+      const lowerCasedEmailInput = emailInput.toLowerCase();
+
+      // eslint-disable-next-line
+      const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (emailInput === "")
+         this.setState({
+            emailError: "Please enter your email address.",
+            hasEmailError: true,
+         });
+      else if (!emailRegex.test(String(lowerCasedEmailInput))) {
+         this.setState({
+            emailError: "Please enter A VALID email address.",
+            hasEmailError: true,
+         });
+      } else {
+         this.setState({ emailError: "", hasEmailError: false });
+      }
+   }
+   checkHasLocalPart(passwordInput, emailInput) {
+      const localPart = emailInput.split("@")[0];
+      if (localPart === "") return false;
+      else if (localPart.length < 4) return false;
+      else return passwordInput.includes(localPart);
+   }
+   async setPasswordState(passwordInput, emailInput) {
+      console.log(passwordInput);
+
+      const uniqChars = [...new Set(passwordInput)];
+      if (passwordInput === "") {
+         this.setState({
+            passwordError: "Please enter a password.",
+            hasPasswordError: true,
+         });
+      } else if (passwordInput.length < 9) {
+         this.setState({
+            passwordError: "Password must be 9 characters.",
+            hasPasswordError: true,
+         });
+      } else if (this.checkHasLocalPart(passwordInput, emailInput)) {
+         this.setState({
+            passwordError: "Password cannot contain your email address.",
+            hasPasswordError: true,
+         });
+      } else if (uniqChars.length < 3) {
+         this.setState({
+            passwordError:
+               "Password msut contain more then 3 unique characters.",
+            hasPasswordError: true,
+         });
+      } else {
+         this.setState({ passwordError: "", hasPasswordError: false });
+      }
+   }
+
+   async validateAndCreateUser() {
+      console.log("clicked");
+      const emailInput = document.getElementById("email").value;
+      const passwordInput = document.getElementById("password").value;
+
+      console.log(emailInput);
+      await this.setEmailState(emailInput);
+      await this.setPasswordState(passwordInput, emailInput);
+      if (
+         this.state.hasEmailError === false &&
+         this.state.hasPasswordError === false
+      ) {
+         const user = {
+            id: getUuid(),
+            email: emailInput,
+            password: hash(passwordInput),
+            createdOn: Date.now(),
+         };
+         console.log(user);
+         this.props.history.push("/Home");
+      }
+   }
+   render() {
+      return (
+         <>
+            <h4 className="text-center">Lets Sign In</h4>
+
+            <input
+               className={classnames({
+                  "form-control": true,
+                  "is-invalid": this.state.hasEmailError,
+               })}
+               placeholder="Enter Email"
+               id="email"
+               type="email"
+            />
+
+            {this.state.hasEmailError && (
+               <small className="text-danger float-left mb-2">
+                  {this.state.emailError}
+               </small>
+            )}
+
+            <input
+               className={classnames({
+                  "form-control": true,
+                  "mt-3": true,
+                  "is-invalid": this.state.hasPasswordError,
+               })}
+               placeholder="Enter Password"
+               id="password"
+               type="password"
+            />
+            {this.state.hasPasswordError && (
+               <div className="text-danger float-left">
+                  <small>{this.state.passwordError}</small>
+               </div>
+            )}
+
+            <button
+               className="btn btn-primary w-100 mt-3"
+               onClick={() => {
+                  this.validateAndCreateUser();
+               }}
+            >
+               Lets Go I'm Starving!
+            </button>
+         </>
+      );
+   }
+}
+export default withRouter(SignInUi);
