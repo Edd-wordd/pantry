9bb481bea7874b8e7d41a0832abf777c26bc702b
Edd-wordd <edward.plasencio@gmail.com>
2020-08-01 17:02:47 -0700

    add navbar to landing page

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -4,11 +4,60 @@ import AppTemplate from "../ui/AppTemplate";
 import SignUp from "../ui/SignUp";
 import { Link } from "react-router-dom";
 import pantryLogo from "../../img/pantryLogo.png";
+import { Nav, NavDropdown, Navbar } from "react-bootstrap";
 
 export default function Landing() {
    return (
       <div>
-         <nav className="navbar navbar-expand-md navbar-light fixed-top">
+         <Navbar
+            collapseOnSelect
+            expand="lg"
+            bg="dark"
+            variant="dark"
+            fixed="top"
+         >
+            <Navbar.Brand href="#home">
+               PANTRY
+               {/* <img
+                  className="logo py-0"
+                  id="brandLogo"
+                  src={pantryLogo}
+                  width="30px"
+                  height="30px"
+                  alt="pantry logo"
+               /> */}
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+               <Nav className="ml-auto">
+                  <Nav.Link href="#features">About Us</Nav.Link>
+                  <Nav.Link href="/pantry">Pantry</Nav.Link>
+                  <NavDropdown title="Recipes" id="collasible-nav-dropdown">
+                     <NavDropdown.Item href="/recipes">
+                        Add Recipes
+                     </NavDropdown.Item>
+                     <NavDropdown.Item href="/home">
+                        View All Recipes
+                     </NavDropdown.Item>
+                     <NavDropdown.Item href="#action/3.3">
+                        Something
+                     </NavDropdown.Item>
+                     <NavDropdown.Divider />
+                     <NavDropdown.Item href="#action/3.4">
+                        Separated link
+                     </NavDropdown.Item>
+                  </NavDropdown>
+               </Nav>
+               <Nav>
+                  <Nav.Link href="/">LogOut</Nav.Link>
+                  {/* <Nav.Link eventKey={2} href="#memes">
+                     Dank memes
+                  </Nav.Link> */}
+               </Nav>
+            </Navbar.Collapse>
+         </Navbar>
+
+         {/* <nav className="navbar navbar-expand-md navbar-light fixed-top">
             <Link to="/" className="navbar-brand">
                <img
                   id="brandLogo"
@@ -39,7 +88,7 @@ export default function Landing() {
                   </li>
                </ul>
             </div>
-         </nav>
+         </nav> */}
          <AppTemplate>
             <div className="offset-1 col-10 offset-sm-2 col-sm-8 offset-md-1 col-md-5 offset-lg-2 col-lg-4 offset-xl-1 col-xl-6">
                <div className="landing-text carousel-caption">
